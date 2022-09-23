ece7b51dbb50f6c34bd168f91f858a02c4aecc75
Mateusz Sójka <MateuszSojka@proton.me>
2022-09-23 12:58:36 +0200

    Introduced EmployeeMapper helper class

---
+++ src/main/java/com/sojka/employeemanager/employee/domain/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.sojka.employeemanager.employee.domain;
+
+import com.sojka.employeemanager.employee.dto.EmployeeDto;
+
+public interface EmployeeMapper {
+
+    default Employee mapToEmployee(EmployeeDto employeeDto) {
+        return Employee.builder()
+                .firstName(employeeDto.getFirstName())
+                .secondName(employeeDto.getSecondName())
+                .lastName(employeeDto.getLastName())
+                .birthDate(employeeDto.getBirthDate())
+                .personalId(employeeDto.getPersonalId())
+                .build();
+    }
+
+    default EmployeeDto mapToEmployeeDto(Employee employee) {
+        return EmployeeDto.builder()
+                .firstName(employee.getFirstName())
+                .secondName(employee.getSecondName())
+                .lastName(employee.getLastName())
+                .birthDate(employee.getBirthDate())
+                .personalId(employee.getPersonalId())
+                .build();
+    }
+}
