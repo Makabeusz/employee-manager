bc5ba7ba45725e6ba481661ebdf6f7c0791a2f48
Mateusz Sójka <MateuszSojka@proton.me>
2022-09-27 16:09:02 +0200

    Removed spring-boot-starter-data-jpa from build.gradle. This will be native sql app - no Hibernate.

--- src/main/java/com/sojka/employeemanager/employee/domain/Employee.java
+++ src/main/java/com/sojka/employeemanager/employee/domain/Employee.java
@@ -5,32 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "employee")
 public class Employee {
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
-    @Column(name = "first_name")
     private String firstName;
-    @Column(name = "second_name")
     private String secondName;
-    @Column(name = "last_name")
     private String lastName;
-    @Column(name = "birth_date")
     private String birthDate;
-    @Column(name = "personal_id")
     private String personalId;
 
 }
