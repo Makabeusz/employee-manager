007d181ef8516db8c26f23216caaf37f66c7569c
Mateusz Sójka <MateuszSojka@proton.me>
2022-09-27 14:56:32 +0200

    Annotated Employee entity object and connected data source - MySql localhosted database.

--- src/main/java/com/sojka/employeemanager/employee/domain/Employee.java
+++ src/main/java/com/sojka/employeemanager/employee/domain/Employee.java
@@ -1,17 +1,36 @@
 package com.sojka.employeemanager.employee.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "employee")
 public class Employee {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "second_name")
     private String secondName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "birth_date")
     private String birthDate;
+    @Column(name = "personal_id")
     private String personalId;
 
 }
