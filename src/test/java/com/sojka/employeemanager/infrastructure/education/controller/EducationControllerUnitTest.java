78004f4aec9e7f2bb31e23cf1b0b709629a119fb
Mateusz Sójka <MateuszSojka@proton.me>
2022-10-01 14:52:37 +0200

    Added SampleEducationDegree helper interface in tests. Preparing mocked config class for EducationControllerUnitTest

--- src/test/java/com/sojka/employeemanager/infrastructure/education/controller/EducationControllerUnitTest.java
+++ src/test/java/com/sojka/employeemanager/infrastructure/education/controller/EducationControllerUnitTest.java
@@ -1,6 +1,11 @@
 package com.sojka.employeemanager.infrastructure.education.controller;
 
 import com.sojka.employeemanager.config.MessageSourceConfig;
+import com.sojka.employeemanager.infrastructure.InMemoryTestDatabase;
+import com.sojka.employeemanager.infrastructure.education.domain.Education;
+import com.sojka.employeemanager.infrastructure.education.domain.repository.EducationRepository;
+import com.sojka.employeemanager.infrastructure.education.domain.service.EducationService;
+import com.sojka.employeemanager.infrastructure.education.dto.SampleEducationDegree;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +14,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 @WebMvcTest(EducationController.class)
 @ContextConfiguration(classes = EducationControllerUnitTest.MockMvcConfig.class)
-class EducationControllerUnitTest {
+class EducationControllerUnitTest implements SampleEducationDegree {
 
     private MockMvc mockMvc;
 
@@ -22,9 +28,27 @@ class EducationControllerUnitTest {
     }
 
     @Import(MessageSourceConfig.class)
-    static class MockMvcConfig {
+    static class MockMvcConfig implements SampleEducationDegree {
 
-//        @Bean
+        private InMemoryTestDatabase<Education> repository =
+                InMemoryTestDatabase.of(firstEmployeeBachelorDegree(), firstEmployeeMasterDegree(), secondEmployeeSecondaryDegree());
+
+        @Bean
+        EducationService educationService() {
+            return new EducationService() {
+
+            };
+        }
+
+        @Bean
+        EducationController educationController(EducationService service) {
+            return new EducationController(service);
+        }
+
+        @Bean
+        EducationRepository educationRepository() {
+            return mock(EducationRepository.class);
+        }
 
     }
 }
---
+++ src/test/java/com/sojka/employeemanager/infrastructure/education/dto/SampleEducationDegree.java
@@ -0,0 +1,41 @@
+package com.sojka.employeemanager.infrastructure.education.dto;
+
+import com.sojka.employeemanager.infrastructure.education.domain.Education;
+
+public interface SampleEducationDegree {
+
+    default Education firstEmployeeBachelorDegree() {
+        return Education.builder()
+                .id("1")
+                .degree("Bachelor")
+                .schoolName("University of London")
+                .address("Gower Street, London, WC1E 6BT")
+                .startDate("2010-10-01")
+                .finishDate("2013-07-02")
+                .build();
+    }
+
+    default Education firstEmployeeMasterDegree() {
+        return Education.builder()
+                .id("1")
+                .degree("Master")
+                .schoolName("University of London")
+                .address("Gower Street, London, WC1E 6BT")
+                .startDate("2013-10-01")
+                .finishDate("2015-06-29")
+                .build();
+    }
+
+    default Education secondEmployeeSecondaryDegree() {
+        return Education.builder()
+                .id("1")
+                .degree("Secondary")
+                .schoolName("College of Lake County")
+                .address("19351 W Washington St., Grayslake, IL, 60030-1198")
+                .startDate("1990-10-01")
+                .finishDate("2015-06-29")
+                .build();
+    }
+
+
+}
