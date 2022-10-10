dd8c5c8a7c27419621b4a7f8ce727788e783b248
Mateusz Sójka <MateuszSojka@proton.me>
2022-10-10 09:33:41 +0200

    Implemented security DTOs - JwtUserDto and LoginRequestDto.

--- src/main/java/com/sojka/employeemanager/security/dto/JwtUserDto.java
+++ src/main/java/com/sojka/employeemanager/security/dto/JwtUserDto.java
@@ -1,4 +1,14 @@
 package com.sojka.employeemanager.security.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class JwtUserDto {
+
+    private final String jwt;
+    private final String username;
 }
--- src/main/java/com/sojka/employeemanager/security/dto/LoginRequestDto.java
+++ src/main/java/com/sojka/employeemanager/security/dto/LoginRequestDto.java
@@ -1,4 +1,16 @@
 package com.sojka.employeemanager.security.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
 public class LoginRequestDto {
+
+    @NotBlank
+    private final String username;
+    @NotBlank
+    private final String password;
 }
