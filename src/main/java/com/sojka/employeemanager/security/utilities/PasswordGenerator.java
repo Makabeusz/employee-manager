fddab258f6bed9bc0f5e63cd1b76183b3ddba34d
Mateusz Sójka <MateuszSojka@proton.me>
2022-10-25 10:35:35 +0200

    Continued on registration feature:
    - PasswordGenerator is now final.
    - Implemented unit tests for PasswordGenerator.

--- src/main/java/com/sojka/employeemanager/security/utilities/PasswordGenerator.java
+++ src/main/java/com/sojka/employeemanager/security/utilities/PasswordGenerator.java
@@ -6,16 +6,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PasswordGenerator {
-
-
+public final class PasswordGenerator {
 
     public static String randomSecurePassword() {
         String upperCaseLetters = RandomStringUtils.random(2,65,90,true,true);
         String lowerCaseLetters = RandomStringUtils.random(2,97,122,true,true);
         String digits = RandomStringUtils.randomNumeric(2);
         String specialChars = RandomStringUtils.random(2,33,47,false,false);
-        String totalChars = RandomStringUtils.randomAlphabetic(2);
+        String totalChars = RandomStringUtils.randomAlphanumeric(2);
         StringBuilder combined = new StringBuilder(upperCaseLetters)
                 .append(lowerCaseLetters)
                 .append(digits)
--- src/test/java/com/sojka/employeemanager/security/utilities/PasswordGeneratorUnitTest.java
+++ src/test/java/com/sojka/employeemanager/security/utilities/PasswordGeneratorUnitTest.java
@@ -3,14 +3,28 @@ package com.sojka.employeemanager.security.utilities;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordGeneratorUnitTest {
 
     @Test
-    void randomly_generated_password_have_two_characters_of_lowercase_uppercase_digits_and_special_signs() {
+    void randomly_generated_password_have_at_least_two_characters_of_lowercase_uppercase_digits_and_specials() {
         String password = PasswordGenerator.randomSecurePassword();
 
-        password.matches(".");
+        boolean containsAtLeastTwoUppers = password.matches(".*\\p{Upper}.*\\p{Upper}.*");
+        boolean containsAtLeastTwoLowers = password.matches(".*\\p{Lower}.*\\p{Lower}.*");
+        boolean containsAtLeastTwoSpecials = password.matches(".*\\p{Punct}.*\\p{Punct}.*");
+        boolean containsAtLeastTwoDigits = password.matches(".*\\d.*\\d.*");
+
+        assertThat(containsAtLeastTwoUppers).isTrue();
+        assertThat(containsAtLeastTwoLowers).isTrue();
+        assertThat(containsAtLeastTwoSpecials).isTrue();
+        assertThat(containsAtLeastTwoDigits).isTrue();
+    }
+
+    @Test
+    void randomly_generated_password_is_ten_characters_long() {
+        String password = PasswordGenerator.randomSecurePassword();
+
+        assertThat(password.length()).isEqualTo(10);
     }
 }
